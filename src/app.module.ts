8d03e8e8d0be1e145d026a74f33b9460d5895350
eckon <niklas.meyer95@hotmail.de>
2021-11-19 18:26:42 +0100

    feat: add different db config for live and develop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,43 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollectionModule } from './collection/collection.module';
 import { ReviewModule } from './review/review.module';
 
 @Module({
   imports: [
-    // TODO: this needs to handle local and production environments
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const url = configService.get('DATABASE_URL');
+        // TODO: improve config of db
+
+        // live config
+        if (url) {
+          return {
+            type: 'postgres',
+            url,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+            autoLoadEntities: true,
+            synchronize: true,
+          };
+        }
+
+        // dev config
+        return {
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'root',
+          password: 'root',
+          database: 'db',
+          autoLoadEntities: true,
+          synchronize: true,
+        };
       },
-      autoLoadEntities: true,
-      synchronize: true,
+      inject: [ConfigService],
     }),
     CollectionModule,
     ReviewModule,
